82e7a4de793b437dade4737f93ecc0616c35d4a7
Theo <theopvalente@gmail.com>
2025-06-06 18:22:01 -0300

    commit1

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import android.widget.TextView
+import android.text.SpannableString
+import android.text.style.StyleSpan
+import android.graphics.Typeface
+import android.text.Spannable
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Permite que o conteúdo vá atrás das barras do sistema
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        enableEdgeToEdge()
+
+        setContentView(R.layout.activity_login)
+        val textView = findViewById<TextView>(R.id.seu_textview)
+        val texto = "Não possui conta? Registre-se"
+        val spannable = SpannableString(texto)
+        spannable.setSpan(
+            StyleSpan(Typeface.BOLD),
+            texto.indexOf("Registre-se"),
+            texto.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        textView.text = spannable
+    }
+}
